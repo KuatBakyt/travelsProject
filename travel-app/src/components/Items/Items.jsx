c12d1beda791ac9fc3d6a3095e81ec289a3ba469
BakytKuat <MY_NAME@example.com>
2024-04-24 16:18:51 +0500

    add input search

---
+++ travel-app/src/components/Items/Items.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Items() {
+  return (
+    <div className='items-content'>
+        <div className='container'>
+          
+        </div>
+    </div>
+  )
+}
+
+export default Items
--- travel-app/src/components/Main/Main.jsx
+++ travel-app/src/components/Main/Main.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
 import '../../allcss/main.css'
+import Items from '../Items/Items'
 
 function Main() {
   return (
-    <div className='main-content'>
-      <div className='container'>
-        <main>
-          <div className='main-title'>
-            <h1>ИНДИВИДУАЛЬНЫЕ ЭКСКУРСИИ И ТУРЫ В АЛМАТЫ</h1>
-            <h5>Мы предлагаем вам доступную цену и отличный сервис</h5>
-            <div className='main-support'>
-              <div className='main-btn'>Предложения</div>
-              <a href='tel: +7 747 620 28 79' className='phone'><img src="../img/phone.png" alt="" /> +7 747 620 28 79</a>
+    <div>
+      <div className='main-content'>
+        <div className='container'>
+          <main>
+            <div className='main-title'>
+              <h1>ИНДИВИДУАЛЬНЫЕ ЭКСКУРСИИ И ТУРЫ В АЛМАТЫ</h1>
+              <h5>Мы предлагаем вам доступную цену и отличный сервис</h5>
+              <div className='main-support'>
+                <div className='main-btn'>Предложения</div>
+                <a href='tel: +7 747 620 28 79' className='phone'><img src="../img/phone.png" alt="" /> +7 747 620 28 79</a>
+              </div>
             </div>
-          </div>
-          <div className='main-social'>
-            <a href="#"><img src="../img/twitter.png" alt="twitter" /></a>
-            <a href="#"><img src="../img/insta.png" alt="insta" /></a>
-            <a href="#"> <img src="../img/facebook.png" alt="facebook" /></a>
-          </div>
-        </main>
+            <div className='main-social'>
+              <a href="#"><img src="../img/twitter.png" alt="twitter" /></a>
+              <a href="#"><img src="../img/insta.png" alt="insta" /></a>
+              <a href="#"> <img src="../img/facebook.png" alt="facebook" /></a>
+            </div>
+          </main>
+        </div>
       </div>
+      <div className='search-item'>
+          <img src="../img/search.png" alt="" />
+          <input type="text" placeholder='Поиск туров...'/>
+      </div>
+    <Items />
     </div>
+
   )
 }
 
